b49ffcb5fc9f0d9398e06c40014052793e634f54
aryanraw <aryan.rawat076@gmail.com>
2023-03-16 08:15:43 +0530

    hello world

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import asyncio
 import websockets
+import json
 
 async def main():
     async with websockets.connect("wss://ws.blockchain.info/inv") as client:
-        print("[main] Connected to wss://ws.blockchain.info/inv" )
-
+        print("[main] Connected to wss://ws.blockchain.info/inv")
         cmd = '{"op":"unconfirmed_sub"}'
         print('[main] Send:', cmd)
         await client.send(cmd)
-        await client.recv()
         reav = client.recv()
         print('[main] Recv:', await reav)
+        return reav
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
+
+fileobj = open("list.json","w")
+json.dump(main(),index=6)
+fileobj.close()
